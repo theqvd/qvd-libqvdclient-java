876d7a8ffe526f9b0aa81549ee6ee72d1847a8a0
Nito Martinez <Nito@Qindel.ES>
2016-02-12 10:12:56 +0100

    #1928 Add README and license information

--- jni/src/main/java/com/theqvd/client/jni/QvdclientWrapper.java
+++ jni/src/main/java/com/theqvd/client/jni/QvdclientWrapper.java
@@ -22,6 +22,26 @@ package com.theqvd.client.jni;
  * @author Nito@Qindel.ES
  *
  */
+
+/*
+ * libqvdclient-java QvdclientWrapper.java
+ *
+ * Copyright (C) 2016  theqvd.com trade mark of Qindel Formacion y Servicios SL
+ *
+ * libqvdclient is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 public class QvdclientWrapper {
 	private Qvdclient qvdclient;
 
